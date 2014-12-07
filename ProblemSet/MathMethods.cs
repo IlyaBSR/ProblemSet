5b6d4976198fa2bf1a3fb6e27ad05d644f7b5898
Ilya Kozavchinsky <ilyakoz@outlook.com>
2014-12-06 17:26:37 -0800

    Reverse an integer

---
+++ ProblemSet/MathMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSet
+{
+    public class MathMethods
+    {
+
+        public static uint ReverseInt(uint inputInt)
+        {
+            uint output = 0;
+
+            while(inputInt > 0)
+            {
+                output = output * 10 + inputInt % 10;
+                inputInt /= 10;
+            }
+
+            return output;
+        }
+    }
+}
---
+++ UnitTests/MathTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProblemSet;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class MathTests
+    {
+        [TestMethod]
+        public void ReverseInt_Basic()
+        {
+            // Arrange
+            uint input = 1234;
+            uint expected = 4321;
+
+            // Act
+            uint output = MathMethods.ReverseInt(input);
+            
+            // Assert
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
